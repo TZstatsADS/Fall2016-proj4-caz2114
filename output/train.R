57eea5ba84d3a62d69f9736cf28399d08681a351
caz2114 <caz2114@barnard.edu>
2016-11-18 13:17:07 -0500

    Update train.R

--- output/train.R
+++ output/train.R
@@ -11,14 +11,28 @@ library("ggplot2")
 library("caret")
 
 
+#source
+setwd("D:/Fall2016-proj4-caz2114/output")
+source("feature.R")
+source("h5_input.R")
+
 #load data file
-setwd("C:/Users/CATHY/OneDrive/Documents/2016-2017 Junior/03 Applied Data Science/Project 4/Fall2016-Proj4/Data")
-load("./kmeans_binded.RData")
+load("../data/fit_topic_model.RData")
 
 
-train <- kmeans_binded[1:2000,]
-test <- kmeans_binded[2001:2350,]
-cl <- factor(fit_topic_model[1:2000])
+#loading h5 file for test
+
+setwd("D:/TestSongFile100/TestSongFile100")
+test_h5 <- loadh5("D:/TestSongFile100/TestSongFile100")
+
+#loading h5 file for train
+setwd("D:/Project4_data/data/data")
+train_h5 <- loadh5("D:/Project4_data/data/data")
+
+
+train <- get.feature(train_h5)
+test <- get.feature(test_h5)
+cl <- factor(fit_topic_model)
 fit_knn <- knn3Train(train,test,cl,k=20,prob=TRUE)
 p.value <-attributes(fit_knn)[[1]]
 grouped <- fit$topics
