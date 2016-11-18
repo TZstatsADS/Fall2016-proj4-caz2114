9be95124f50383b82c635b1f12853563304a4936
caz2114 <caz2114@barnard.edu>
2016-11-18 13:17:30 -0500

    Update h5_input.R

--- doc/h5_input.R
+++ doc/h5_input.R
@@ -1,16 +1,19 @@
 #This file load the h5 files
-setwd("C:/Users/CATHY/OneDrive/Documents/2016-2017 Junior/Applied Data Science/Project 4/Fall2016-Proj4-caz/data")
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
+
+library(rhdf5)
 
 
-h5file_path <- list.files(path = "./", recursive = TRUE)
-h5file <- list()
+loadh5 <- function(filepath){
+  h5file_path <- list.files(path = "./", recursive = TRUE)
+  h5file <- list()
 
-
-for( i in 1:length(h5file_path)){
-  H5close()
-  h5file[[i]] <- h5read(h5file_path[i],"/analysis")
+  for( i in 1:length(h5file_path)){
+    H5close()
+    h5file[[i]] <- h5read(h5file_path[i],"/analysis")
+  }
+  
+  return(h5file) 
 }
 
-
-setwd("C:/Users/CATHY/OneDrive/Documents/2016-2017 Junior/Applied Data Science/Project 4/Fall2016-Proj4-caz/")
-save(h5file,file = "h5file.RData")
